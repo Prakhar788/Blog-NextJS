15c48709c57a0d4d66980da8a47587e37ef6242a
Prakhar <prakhar6601@gmail.com>
2023-09-14 22:49:40 +0530

    Adding new files

---
+++ src/components/loader/Loader.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import styles from "./loader.module.css"
+
+const Loader = () => {
+  return (
+  
+         <div className={styles.hourglassBackground}>
+      <div className={styles.hourglassContainer}>
+        <div className={styles.hourglassCurves}></div>
+        <div className={styles.hourglassCapTop}></div>
+        <div className={styles.hourglassGlassTop}></div>
+        <div className={styles.hourglassSand}></div>
+        <div className={styles.hourglassSandStream}></div>
+        <div className={styles.hourglassCapBottom}></div>
+        <div className={styles.hourglassGlass}></div>
+      </div>
+    </div>
+
+  )
+}
+
+export default Loader
